79a35ac3c1cc56cb25f85b4e549a9d66c32cebcc
Eugenio Pérez <eupm90@gmail.com>
2014-04-09 16:11:29 +0000

    Added global configuration struct

---
+++ config.h
@@ -0,0 +1,34 @@
+/*
+** Copyright (C) 2014 Eneo Tecnologia S.L.
+** Author: Eugenio Perez <eupm90@gmail.com>
+**
+** This program is free software; you can redistribute it and/or modify
+** it under the terms of the GNU General Public License Version 2 as
+** published by the Free Software Foundation.  You may not use, modify or
+** distribute this program under any other version of the GNU General
+** Public License.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program; if not, write to the Free Software
+** Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+*/
+
+#pragma once
+
+#include <stdint.h>
+
+struct n2kafka_config{
+    unsigned int tcp_threads;
+    unsigned int udp_threads;
+    const char *format;
+    uint16_t listen_port;
+    const char *topic;
+    const char *broker;
+};
+
+extern struct n2kafka_config global_config;
