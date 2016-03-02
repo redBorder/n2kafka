e741e7d82cd56e22773b24bbd5138017a1703506
Eugenio Pérez <eupm90@gmail.com>
2016-03-02 11:36:51 +0000

    Created 0007-mac-partitioner.c

---
+++ tests/0007-mac-partitioner.c
@@ -0,0 +1,122 @@
+#include "rb_json_tests.c"
+#include <librdkafka/rdkafka.h>
+
+#include "../src/decoder/rb_http2k_decoder.c"
+#include "../src/listener/http.c"
+
+#include <assert.h>
+#include <setjmp.h>
+#include <cmocka.h>
+
+static const int32_t default_partition_cnt = 4;
+
+struct test_mac {
+	const char *mac;
+	int32_t idx;
+};
+
+static const struct test_mac test_mac[] = {
+	{.mac = "d2:65:2f:83:68:cd", .idx = 1},
+	{.mac = "97:0f:d4:6e:92:05", .idx = 1},
+	{.mac = "f8:43:aa:3b:e0:ce", .idx = 2},
+	{.mac = "de:fb:64:43:65:ae", .idx = 2},
+	{.mac = "ab:e4:94:bc:61:78", .idx = 0},
+	{.mac = "4a:7e:b9:9c:3b:89", .idx = 1},
+	{.mac = "00:1c:bf:45:7a:68", .idx = 0},
+	{.mac = "c2:cb:7c:3a:9b:a9", .idx = 1},
+	{.mac = "2c:77:58:60:dd:ce", .idx = 2},
+	{.mac = "05:2e:4c:41:84:05", .idx = 1},
+	{.mac = "6a:76:92:dc:84:c6", .idx = 2},
+	{.mac = "fe:12:91:75:19:b8", .idx = 0},
+	{.mac = "05:85:19:42:15:cb", .idx = 3},
+	{.mac = "67:d0:9a:59:9f:5e", .idx = 2},
+	{.mac = "97:70:ba:00:23:a6", .idx = 2},
+	{.mac = "5c:4d:ee:b5:23:6e", .idx = 2},
+	{.mac = "46:c1:04:97:f5:ba", .idx = 2},
+	{.mac = "f7:1f:cd:1b:a9:82", .idx = 2},
+	{.mac = "cd:0b:b6:b8:92:a0", .idx = 0},
+	{.mac = "48:a3:c3:25:da:aa", .idx = 2},
+	{.mac = "90:a3:b5:ef:e9:d1", .idx = 1},
+	{.mac = "d9:43:06:a8:f2:43", .idx = 3},
+	{.mac = "2c:3f:e6:35:bf:b0", .idx = 0},
+	{.mac = "32:dc:d4:4d:65:da", .idx = 2},
+	{.mac = "3a:15:85:35:4c:78", .idx = 0},
+	{.mac = "49:80:d6:34:e5:da", .idx = 2},
+	{.mac = "89:0b:35:43:a2:53", .idx = 3},
+	{.mac = "78:ec:60:96:d4:e2", .idx = 2},
+	{.mac = "c3:ce:a8:7d:89:3c", .idx = 0},
+	{.mac = "83:e1:bc:5d:cd:73", .idx = 3},
+};
+
+static int32_t mac_partitioner (const rd_kafka_topic_t *rkt,
+                                const void *keydata, size_t keylen,
+                                int32_t partition_cnt,
+                                void *rkt_opaque, void *msg_opaque);
+
+/// @note HOOK to know that mac_partitioner could not find a valid MAC
+int32_t rd_kafka_msg_partitioner_random(const rd_kafka_topic_t *rkt,
+                                const void *keydata, size_t keylen,
+                                int32_t partition_cnt,
+                                void *rkt_opaque, void *msg_opaque) {
+	(void)rkt;
+	(void)keydata;
+	(void)keylen;
+	(void)partition_cnt;
+	(void)rkt_opaque;
+	(void)msg_opaque;
+	return 100;
+}
+
+static void samples_test() {
+	const rd_kafka_topic_t *rkt = NULL;
+	void *rkt_opaque = NULL, *msg_opaque = NULL;
+
+	size_t i;
+	for(i=0; i<sizeof(test_mac)/sizeof(test_mac[0]); ++i) {
+		int32_t result = mac_partitioner(rkt,
+		                test_mac[i].mac, strlen(test_mac[i].mac),
+		                default_partition_cnt,
+		                rkt_opaque, msg_opaque);
+
+		assert(result == test_mac[i].idx);
+	}
+}
+
+static void invalid_mac_len_test() {
+	size_t i;
+	const rd_kafka_topic_t *rkt = NULL;
+	void *rkt_opaque = NULL, *msg_opaque = NULL;
+	char mac[sizeof(test_mac[0].mac) + 1];
+	const size_t mac_len = strlen(test_mac[0].mac);
+
+	/// Test all sizes under right size
+	for (i = 0; i <= mac_len + 1; ++i) {
+		if (i == mac_len) {
+			continue;
+		}
+
+		mac[0] = '\0';
+		strncat(mac,test_mac[0].mac,i);
+
+		if (i > mac_len) {
+			mac[mac_len] = '0';
+			mac[i] = '\0';
+		}
+
+		const int32_t result = mac_partitioner(rkt,
+		                mac, strlen(mac),
+		                default_partition_cnt,
+		                rkt_opaque, msg_opaque);
+
+		assert(100 == result);
+	}
+}
+
+int main() {
+	const struct CMUnitTest tests[] = {
+		cmocka_unit_test(samples_test),
+		cmocka_unit_test(invalid_mac_len_test),
+	};
+
+	return cmocka_run_group_tests(tests, NULL, NULL);
+}
