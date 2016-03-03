f02f8cb9b83d5cc0ecf53f980563e62d1377f31b
Eugenio Pérez <eupm90@gmail.com>
2016-03-03 16:21:59 +0000

    Checking messages with premature closing (})

--- tests/0008-http2k-malformed-messages.c
+++ tests/0008-http2k-malformed-messages.c
@@ -107,6 +107,11 @@ static void check_rb_decoder_two_keys(struct rb_session **sess,
 	free(rkm.payload);
 }
 
+static void check_rb_decoder_premature_closing(struct rb_session **sess,
+                void *opaque) {
+	check_rb_decoder_two_keys(sess,opaque);
+}
+
 /// Simple decoding with another enrichment
 static void test_rb_decoder_two_keys() {
 	struct pair mem[3];
@@ -143,6 +148,47 @@ static void test_rb_decoder_two_keys() {
 #undef MESSAGES
 }
 
+/// Trying to decode a JSON closing when you still have not open any json
+static void test_rb_decoder_closing() {
+	struct pair mem[3];
+	keyval_list_t args;
+	keyval_list_init(&args);
+	prepare_args("rb_flow","def","127.0.0.1",mem,RD_ARRAYSIZE(mem),&args);
+
+#define MESSAGES                                                            \
+	X("}{\"client_mac\": \"54:26:96:db:88:02\", "                       \
+		"\"application_name\": \"wwww\", \"sensor_uuid\":\"def\", " \
+		"\"a\":5, \"u\":true}",                                     \
+		check_zero_messages)                                        \
+	X("}}{\"client_mac\": \"54:26:96:db:88:02\", "                      \
+		"\"application_name\": \"wwww\", \"sensor_uuid\":\"def\", " \
+		"\"a\":5, \"u\":true}",                                     \
+		check_zero_messages)                                        \
+	X("}}}{\"client_mac\": \"54:26:96:db:88:02\", "                     \
+		"\"application_name\": \"wwww\", \"sensor_uuid\":\"def\", " \
+		"\"a\":5, \"u\":true}",                                     \
+		check_zero_messages)                                        \
+	/* Free & Check that session has been freed */                      \
+	X(NULL,check_null_session)
+
+	struct message_in msgs[] = {
+#define X(a,fn) {a,sizeof(a)-1},
+		MESSAGES
+#undef X
+	};
+
+	check_callback_fn callbacks_functions[] = {
+#define X(a,fn) fn,
+		MESSAGES
+#undef X
+	};
+
+	test_rb_decoder0(CONFIG_TEST, &args, msgs, callbacks_functions,
+		RD_ARRAYSIZE(msgs), NULL);
+
+#undef MESSAGES
+}
+
 int main() {
 	/// @TODO Need to have rdkafka inited. Maybe this plugin should have it owns rdkafka handler.
 	init_global_config();
@@ -157,6 +203,7 @@ int main() {
 
 	const struct CMUnitTest tests[] = {
 		cmocka_unit_test(test_rb_decoder_two_keys),
+		cmocka_unit_test(test_rb_decoder_closing),
 	};
 
 	return cmocka_run_group_tests(tests, NULL, NULL);
