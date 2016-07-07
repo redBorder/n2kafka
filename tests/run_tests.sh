f809fe850750b9af10cb41fd05c0cc3d0100719f
Diego Fernández Barrera <bigomby@gmail.com>
2016-07-07 13:33:58 +0200

    Update Makefile to take advantajes of -j flag
    - New modular Makefile optimized for concurrent builds and tests
    - Makefiles on test/ folder have been removed
    - Objdeps are updated to match the new location
    - Run test script now receives the files to display tests as arguments
    
    Closes #1

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -3,63 +3,80 @@
 COLUMNS=101                          # Used for center text
 FAILURES=0                           # Number of test failures
 SUCCESSES=0                          # Numer of test successes
-SUPPRESSIONS="valgrind.suppressions" # File containing valgrind suppressions
 TOTAL_MEM_ERRORS=0                   # Errors detected with memcheck
 TOTAL_HELGRIND_ERRORS=0              # Errors detected with helgrind tool
 TOTAL_DRD_ERRORS=0                   # Errors detected with drd
 
-TITLE="RUNNING TESTS"
+# Count number of XML elements that match with string $2 in file $1
+function count_xml_elms {
+  xml-find $1 -name $2 | wc -l
+}
+
+# Extract valgrind error text $2 of file $1
+function valgrind_xml_error_text {
+  TEXT="$(xml-printf '%s' $1 ://error[$2]/what 2>/dev/null)"
+  if [[ -z "$TEXT" ]]; then
+    TEXT="$(xml-printf '%s' $1 ://error[$2]/xwhat/text 2>/dev/null)"
+  fi
+  echo "$TEXT"
+}
+
+TITLE="TESTS RESULTS"
 printf "\n\e[1m\e[34m%*s\e[0m\n" $(((${#TITLE}+$COLUMNS)/2)) "$TITLE"
 printf "\e[1m\e[34m=====================================================================================================\e[0m\n"
 printf "\n"
 
-# Check for every .test file
-for FILE in *.test; do
+while getopts "cvhd" opt; do
+  case $opt in
+    c) DO_CHECKS="y";;
+    v) DO_VALGRIND="y";;
+    h) DO_HELGRIND="y";;
+    d) DO_DRD="y";;
+    \?) echo "Invalid option: -$OPTARG" >&2;;
+  esac
+done
+shift $((OPTIND -1))
 
-  # Save results on XML files
+# Check for every .test file
+for FILE in $*; do
+  # Saved results on XML files
   RESULT_XML="$FILE".xml
   MEM_XML="$FILE".mem.xml
   HELGRIND_XML="$FILE".helgrind.xml
   DRD_XML="$FILE".drd.xml
 
+  TESTCASES=0
   # Count errors
-  MEM_ERRORS=0                        # Errors detected with memcheck
+  MEM_ERRORS=0                        # Errors detected with memcheck tool
   HELGRIND_ERRORS=0                   # Errors detected with helgrind tool
   DRD_ERRORS=0                        # Errors detected with drd
-
-  # Run the tests using CMOCKA and VALGRIND. Skip running tests if "--display-only"
-  # flag is provide. In that case, just parse the XML files and display the info.
-  if [ "$1" != "--display-only" ]; then
-    CMOCKA_XML_FILE="$RESULT_XML" CMOCKA_MESSAGE_OUTPUT=XML ./"$FILE" &>/dev/null
-
-    # If a valgrind supressions file is provided, use it
-    if [ -f "$SUPPRESSIONS" ];then
-      SUPPRESSIONS_VALGRIND_ARG=--suppressions="$SUPPRESSIONS"
-    fi
-
-    /opt/rh/devtoolset-3/root/usr/bin/valgrind $SUPPRESSIONS_VALGRIND_ARG --xml=yes --xml-file="$MEM_XML" ./"$FILE" &>/dev/null
-    /opt/rh/devtoolset-3/root/usr/bin/valgrind $SUPPRESSIONS_VALGRIND_ARG --tool=helgrind --xml=yes --xml-file="$HELGRIND_XML" ./"$FILE" &>/dev/null
-    /opt/rh/devtoolset-3/root/usr/bin/valgrind $SUPPRESSIONS_VALGRIND_ARG --tool=drd --xml=yes --xml-file="$DRD_XML" ./"$FILE" &>/dev/null
-  fi
 
   printf "\e[1m=====================================================================================================\e[0m\n"
   printf "\e[1m%*s\e[0m\n" $(((${#FILE}+$COLUMNS)/2)) "$FILE"
   printf "\e[1m=====================================================================================================\e[0m\n"
 
+  # Get previous information
+  if [[ "x$DO_CHECKS" == "xy" ]]; then
+    TESTCASES=$(count_xml_elms "$RESULT_XML" "testcase")
+  fi
+  if [[ "x$DO_VALGRIND" == "xy" ]]; then
+    MEM_ERRORS="$(count_xml_elms $MEM_XML error)"
+  fi
+  if [[ "x$DO_HELGRIND" == "xy" ]]; then
+    HELGRIND_ERRORS="$(count_xml_elms $HELGRIND_XML error)"
+  fi
+  if [[ "x$DO_DRD" == "xy" ]]; then
+    DRD_ERRORS="$(count_xml_elms $DRD_XML error)"
+  fi
 
   # Check the tests results
-  i=1
-  for _ in $(xml-find $RESULT_XML -name testcase); do
-
+  for i in $(seq 1 $TESTCASES); do
     # Parse the XML
     TIME=$(xml-printf '%s' $RESULT_XML ://testcase[$i]@time)
     NAME=$(xml-printf '%s' $RESULT_XML ://testcase[$i]@name)
     FAIL=$(xml-printf '%s' $RESULT_XML ://testcase[$i])
-    MEM="$(xml-printf '%s' $MEM_XML ://error 2>/dev/null)"
-    HELGRIND="$(xml-printf '%s' $HELGRIND_XML ://error 2>/dev/null)"
-    DRD="$(xml-printf '%s' $DRD_XML ://error 2>/dev/null)"
 
-    if [ -z "$FAIL" ] && [ -z "$MEM" ] && [ -z "$HELGRIND" ] && [ -z "$DRD" ]; then
+    if [ -z "$FAIL" ] && [ $MEM_ERRORS -eq 0 ] && [ $HELGRIND_ERRORS -eq 0 ] && [ $DRD_ERRORS -eq 0 ]; then
       # No errors at all
       printf "\t\e[32m ✔ %s \e[0m(%sms)\e[0m\n" "$NAME" "$TIME"
       ((SUCCESSES++))
@@ -67,55 +84,30 @@ for FILE in *.test; do
       # Tests are ok but valgrind found some issues
       printf "\t\e[33m ✔ %s \e[0m(%sms)\e[0m\n" "$NAME" "$TIME"
       ((SUCCESSES++))
-
-      # Memory issues detected
-      if [ -n "$MEM" ]; then
-        ((MEM_ERRORS++))
-        ((TOTAL_MEM_ERRORS++))
-      fi
-
-      # Helgrind issues detected
-      if [ -n "$HELGRIND" ]; then
-        ((HELGRIND_ERRORS++))
-        ((TOTAL_HELGRIND_ERRORS++))
-      fi
-
-      # Drd issues detected
-      if [ -n "$DRD" ]; then
-        ((DRD_ERRORS++))
-        ((TOTAL_DRD_ERRORS++))
-      fi
     else
       # Errors found in tests
       printf "\t\e[1m\e[31m ✘ %s\e[0m\n" "$NAME"
       printf "\t\t\e[31m• %s\e[0m\n" "$FAIL"
       ((FAILURES++))
     fi
-
-    ((i++))
   done
 
   # Check the valgrind memcheck results
-  if [ "$MEM_ERRORS" -gt 0 ] ; then
+  if [[ $MEM_ERRORS -gt 0 ]] ; then
     printf '\n'
     printf "\t\e[1m---------------------------------------------------------------------------------------------\e[0m\n"
     printf "\t\e[1m Memory issues\n"
     printf "\t\e[1m---------------------------------------------------------------------------------------------\e[0m\n"
 
     LOST=0 # Count memory lost due to memory leaks
-    ERRORS="$(xml-printf '%s' $MEM_XML ://error/kind 2>/dev/null)"
-
-    i=1
-    for _ in $ERRORS; do
+    for i in $(seq 1 $MEM_ERRORS); do
       # Display error
-      TEXT="$(xml-printf '%s' $MEM_XML ://error[$i]/xwhat/text 2>/dev/null)"
+      TEXT="$(valgrind_xml_error_text $MEM_XML $i)"
       printf "\t\e[31m • %s\e[0m\n" "$TEXT"
 
       # Count bytes lost due to this error
       BYTES="$(xml-printf '%s' $MEM_XML ://error[$i]/xwhat/leakedbytes 2>/dev/null)"
       ((LOST+=BYTES))
-
-      ((i++))
     done
 
     printf "\n"
@@ -124,41 +116,36 @@ for FILE in *.test; do
   fi
 
   # Check the valgrind drd and helgrind tools results
-  if [ "$HELGRIND_ERRORS" -gt 0 ] || [ "$DRD_ERRORS" -gt 0 ]; then
+  if [[ $HELGRIND_ERRORS -gt 0 ]] || [[ $DRD_ERRORS -gt 0 ]]; then
     printf '\n'
     printf "\t\e[1m---------------------------------------------------------------------------------------------\e[0m\n"
     printf "\t\e[1m Concurrency issues\n"
     printf "\t\e[1m---------------------------------------------------------------------------------------------\e[0m\n"
 
-    ERRORS="$(xml-printf '%s' $HELGRIND_XML ://error/kind 2>/dev/null)"
-
-    i=1
-    for _ in $ERRORS; do
+    for i in $(seq 1 $HELGRIND_ERRORS); do
       # Display helgrind error
-      TEXT="$(xml-printf '%s' $HELGRIND_XML ://error[$i]/xwhat/text 2>/dev/null)"
+      TEXT="$(valgrind_xml_error_text $HELGRIND_XML $i)"
       printf "\t\e[31m • \e[1m[HELGRIND] \e[0;31m%s\e[0m\n" "$TEXT"
-
-      ((i++))
     done
 
-    i=1
-    ERRORS="$(xml-printf '%s' $DRD_XML ://error/kind 2>/dev/null)"
-    for _ in $ERRORS; do
+    for i in $(seq 1 $DRD_ERRORS); do
       # Display drd error
-      TEXT="$(xml-printf '%s' $DRD_XML ://error[$i]/what 2>/dev/null)"
+      TEXT="$(valgrind_xml_error_text $DRD_XML $i)"
       printf "\t\e[31m • \e[1m[DRD] \e[0;31m%s\e[0m\n" "$TEXT"
-
-      ((i++))
     done
     printf "\t\e[1m---------------------------------------------------------------------------------------------\e[0m\n"
   fi
+
+  TOTAL_MEM_ERRORS=$((TOTAL_MEM_ERRORS + MEM_ERRORS))
+  TOTAL_HELGRIND_ERRORS=$((TOTAL_HELGRIND_ERRORS + HELGRIND_ERRORS))
+  TOTAL_DRD_ERRORS=$((TOTAL_DRD_ERRORS + DRD_ERRORS))
 
   printf "\n"
 done
 
 # Show a digest of all errors
-TOTAL_CONCURRENCY_ERRORS=$((TOTAL_HELGRIND_ERRORS + TOTAL_DRD_ERRORS))
-TOTAL_ERRORS=$((TOTAL_MEM_ERRORS + TOTAL_CONCURRENCY_ERRORS))
+CONCURRENCY_ERRORS=$((TOTAL_HELGRIND_ERRORS + TOTAL_DRD_ERRORS))
+TOTAL_ERRORS=$((FAILURES + TOTAL_MEM_ERRORS + CONCURRENCY_ERRORS))
 
 if [ $FAILURES -gt 0 ]; then
   COLOR="\e[1m\e[31m"
@@ -171,7 +158,7 @@ else
   TITLE="PASSED"
 fi
 
-RESULT=$(printf "SUCESSES: %s | FAILURES: %s | MEMORY: %s | CONCURRENCY: %s\e[0m\n" "$SUCCESSES" "$FAILURES" "$TOTAL_MEM_ERRORS" "$TOTAL_CONCURRENCY_ERRORS")
+RESULT=$(printf "SUCESSES: %s | FAILURES: %s | MEMORY: %s | CONCURRENCY: %s\e[0m\n" "$SUCCESSES" "$FAILURES" "$TOTAL_MEM_ERRORS" "$CONCURRENCY_ERRORS")
 echo -e "$COLOR=====================================================================================================\e[0m"
 printf "$COLOR%*s\e[0m\n" $(((${#TITLE}+$COLUMNS)/2)) "$TITLE"
 printf "\n"
