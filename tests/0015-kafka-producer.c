f6aae8e220a3415ee5a7abbbb3ea5d775bea2aa2
Diego Fernández Barrera <bigomby@gmail.com>
2016-07-20 12:47:16 +0200

    :white_check_mark: Add TCP message sending to 0015

--- tests/0015-kafka-producer.c
+++ tests/0015-kafka-producer.c
@@ -8,6 +8,8 @@
 #include <string.h>
 #include <unistd.h>
 
+#include <arpa/inet.h>
+
 #include <setjmp.h>
 #include <stdarg.h>
 #include <stddef.h>
@@ -16,22 +18,21 @@
 
 #include <librdkafka/rdkafka.h>
 
-#define EXPECTED_MESSAGE                                                       \
-  "{\"message\":\"Hello "                                                      \
-  "world\",\"a\":1,\"b\":\"c\",\"d\":true,\"e\":null,\"a_org\":10}"
-#define EXPECTED_MESSAGE_2                                                     \
-  "{\"message\":\"Sayounara "                                                  \
-  "baby\",\"a\":1,\"b\":\"c\",\"d\":true,\"e\":null,\"a_org\":10}"
-#define SENT_MESSAGE "{\"message\": \"Hello world\"}"
-#define SENT_MESSAGE_2 "{\"message\": \"Sayounara baby\"}"
-#define N_MESSAGES_EXPECTED 2
+#define SENT_MESSAGE_HTTP_1 "{\"message\": \"Hello world HTTP\"}"
+#define SENT_MESSAGE_HTTP_2 "{\"message\": \"Sayounara baby HTTP\"}"
+#define SENT_MESSAGE_TCP_1 "{\"message\": \"Hello world TCP\"}"
+#define SENT_MESSAGE_TCP_2 "{\"message\": \"Sayounara baby TCP\"}"
+#define N_MESSAGES_EXPECTED 10
 #define VALID_UUID "/abc"
 #define VALID_URL "http://localhost:2057/rbdata"
 #define VALID_TOPIC "/rb_flow"
+#define TCP_HOST "127.0.0.1"
+#define TCP_PORT 2056
+#define TCP_MESSAGES_DELAY 5
 
-static rd_kafka_t *rk;
+static rd_kafka_t *init_kafka() {
+  rd_kafka_t *rk;
 
-static void init_kafka() {
   // Kafka
   char errstr[512];
   char *brokers = "eugeniodev:9092";
@@ -89,6 +90,8 @@ static void init_kafka() {
   }
 
   rd_kafka_topic_partition_list_destroy(topics);
+
+  return rk;
 }
 
 /**
@@ -96,10 +99,39 @@ static void init_kafka() {
  * kafka
  */
 static void test_send_message_http() {
-  // Init curl
   CURL *curl;
   CURLcode res;
   long http_code = 0;
+  rd_kafka_t *rk = init_kafka();
+  struct assertion_handler_s *assertion_handler = NULL;
+
+  curl_global_init(CURL_GLOBAL_ALL);
+
+  // Fork n2kafka
+  pid_t pID = fork();
+
+  if (pID == 0) {
+    // Close stdin, stdout, stderr
+    close(0);
+    close(1);
+    close(2);
+    open("/dev/null", O_RDWR);
+    (void)(dup(0) + 1);
+    (void)(dup(0) + 1);
+
+    execlp("../n2kafka", "n2kafka",
+           "../configs_example/n2kafka_tests_http.json", (char *)0);
+    printf("Error executing n2kafka\n");
+    exit(1);
+
+  } else if (pID < 0) {
+    exit(1);
+  }
+
+  // Wait for n2kafka to initialize
+  sleep(1);
+
+  // Init curl
   curl = curl_easy_init();
 
   // Build the URL
@@ -109,24 +141,23 @@ static void test_send_message_http() {
   strcat(url, VALID_TOPIC);
 
   // Use the assertion_handler to proccess assertions asynchronously
-  struct assertion_handler_s *assertion_handler = NULL;
   assertion_handler = assertion_handler_new();
 
   if (curl) {
     // Push an assertion to the assertion handler with the expected message
     struct assertion_e *assertion =
         (struct assertion_e *)malloc(sizeof(struct assertion_e));
-    assertion->str = strdup(EXPECTED_MESSAGE);
+    assertion->str = strdup(SENT_MESSAGE_HTTP_1);
     assertion_handler_push_assertion(assertion_handler, assertion);
 
     struct assertion_e *assertion2 =
         (struct assertion_e *)malloc(sizeof(struct assertion_e));
-    assertion2->str = strdup(EXPECTED_MESSAGE_2);
+    assertion2->str = strdup(SENT_MESSAGE_HTTP_2);
     assertion_handler_push_assertion(assertion_handler, assertion2);
 
     // Send message via curl
     curl_easy_setopt(curl, CURLOPT_URL, url);
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, SENT_MESSAGE);
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, SENT_MESSAGE_HTTP_1);
     res = curl_easy_perform(curl);
     curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
 
@@ -136,7 +167,7 @@ static void test_send_message_http() {
 
     // Send message 2 via curl
     curl_easy_setopt(curl, CURLOPT_URL, url);
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, SENT_MESSAGE_2);
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, SENT_MESSAGE_HTTP_2);
     res = curl_easy_perform(curl);
     curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
 
@@ -150,14 +181,13 @@ static void test_send_message_http() {
   free(url);
 
   // Try to read the message from kafka and push it to the assertion handler
-  int n_messages = 0;
-  while (n_messages < N_MESSAGES_EXPECTED) {
+  int i = 0;
+  for (i = 0; i < N_MESSAGES_EXPECTED; i++) {
     rd_kafka_message_t *rkmessage;
     struct value_e *value;
 
     rkmessage = rd_kafka_consumer_poll(rk, 1000);
-    if (rkmessage->len > 0) {
-      n_messages++;
+    if (rkmessage != NULL && rkmessage->len > 0) {
       value = (struct value_e *)malloc(sizeof(struct value_e));
       value->str = malloc((rkmessage->len + 1) * sizeof(char));
       memmove(value->str, rkmessage->payload, rkmessage->len);
@@ -168,17 +198,37 @@ static void test_send_message_http() {
     }
   }
 
+  // Close consumer
+  rd_kafka_consumer_close(rk);
+  sleep(1);
+
+  // Clean consumer
+  rd_kafka_destroy(rk);
+
+  // Stop n2kafka
+  kill(pID, SIGINT);
+  curl_global_cleanup();
+
   // Assert pending messages
   assert_int_equal(assertion_handler_assert(assertion_handler), 0);
   assertion_handler_destroy(assertion_handler);
 }
 
-int main() {
-  init_kafka();
-  curl_global_init(CURL_GLOBAL_ALL);
+/**
+ * Send a message using a TCP socket and expect to receive the enriched message
+ * via kafka
+ */
+static void test_send_message_tcp() {
+  int sock;
+  struct sockaddr_in server;
+  struct assertion_handler_s *assertion_handler = NULL;
+
+  rd_kafka_t *rk = init_kafka();
+  if (rk == NULL) {
+    exit(1);
+  }
 
   // Fork n2kafka
-  printf("Initializing n2kafka\n");
   pid_t pID = fork();
 
   if (pID == 0) {
@@ -190,8 +240,8 @@ int main() {
     (void)(dup(0) + 1);
     (void)(dup(0) + 1);
 
-    execlp("../n2kafka", "n2kafka",
-           "../configs_example/n2kafka_config_rbhttp.json", (char *)0);
+    execlp("../n2kafka", "n2kafka", "../configs_example/n2kafka_tests_tcp.json",
+           (char *)0);
     printf("Error executing n2kafka\n");
     exit(1);
 
@@ -202,16 +252,85 @@ int main() {
   // Wait for n2kafka to initialize
   sleep(1);
 
-  const struct CMUnitTest tests[] = {cmocka_unit_test(test_send_message_http)};
-  const int res = cmocka_run_group_tests(tests, NULL, NULL);
+  // Use the assertion_handler to proccess assertions asynchronously
+  assertion_handler = assertion_handler_new();
+
+  sock = socket(AF_INET, SOCK_STREAM, 0);
+  if (sock == -1) {
+    perror("Could not create socket. Error");
+    exit(1);
+  }
+
+  server.sin_addr.s_addr = inet_addr(TCP_HOST);
+  server.sin_family = AF_INET;
+  server.sin_port = htons(TCP_PORT);
+
+  // Connect to remote server
+  if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
+    perror("Connection failed. Error");
+    exit(1);
+  }
+
+  // Send some data
+  if (send(sock, SENT_MESSAGE_TCP_1, strlen(SENT_MESSAGE_TCP_1), 0) < 0) {
+    perror("Send failed. Error");
+    exit(1);
+  }
+
+  sleep(TCP_MESSAGES_DELAY);
+
+  // Send some data
+  if (send(sock, SENT_MESSAGE_TCP_2, strlen(SENT_MESSAGE_TCP_2), 0) < 0) {
+    perror("Send failed. Error");
+    exit(1);
+  }
+
+  // Add assertions
+  struct assertion_e *assertion = NULL;
+
+  assertion = (struct assertion_e *)malloc(sizeof(struct assertion_e));
+  assertion->str = strdup(SENT_MESSAGE_TCP_1);
+  assertion_handler_push_assertion(assertion_handler, assertion);
+
+  assertion = (struct assertion_e *)malloc(sizeof(struct assertion_e));
+  assertion->str = strdup(SENT_MESSAGE_TCP_2);
+  assertion_handler_push_assertion(assertion_handler, assertion);
+
+  // Try to read the message from kafka and push it to the assertion handler
+  int i = 0;
+  for (i = 0; i < N_MESSAGES_EXPECTED; i++) {
+    rd_kafka_message_t *rkmessage;
+    struct value_e *value;
+
+    rkmessage = rd_kafka_consumer_poll(rk, 1000);
+    if (rkmessage != NULL && rkmessage->len > 0) {
+      value = (struct value_e *)malloc(sizeof(struct value_e));
+      value->str = malloc((rkmessage->len + 1) * sizeof(char));
+      memmove(value->str, rkmessage->payload, rkmessage->len);
+      value->str[rkmessage->len] = '\0';
+      value->len = rkmessage->len;
+      rd_kafka_message_destroy(rkmessage);
+      assertion_handler_push_value(assertion_handler, value);
+    }
+  }
+
+  // Close consumer
+  rd_kafka_consumer_close(rk);
+  sleep(1);
+
+  // Clean consumer
+  rd_kafka_destroy(rk);
 
   kill(pID, SIGINT);
-  sleep(1);
 
-  rd_kafka_consumer_close(rk);
-  rd_kafka_destroy(rk);
-  curl_global_cleanup();
-  sleep(1);
+  // Assert pending messages
+  assert_int_equal(assertion_handler_assert(assertion_handler), 0);
+  assertion_handler_destroy(assertion_handler);
+  close(sock);
+}
 
-  return res;
+int main() {
+  const struct CMUnitTest tests[] = {cmocka_unit_test(test_send_message_http),
+                                     cmocka_unit_test(test_send_message_tcp)};
+  return cmocka_run_group_tests(tests, NULL, NULL);
 }
