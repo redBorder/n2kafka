6e7db5bd4c293f7527e32f75c95eb99c12bca0bd
Ana Rey <anarey@redborder.com>
2016-11-02 16:08:14 +0000

    test 0020: check kafka conf with opaque element

--- tests/0020-meraki-opaque.c
+++ tests/0020-meraki-opaque.c
@@ -135,6 +135,60 @@ static void opaque_check_no_kafka() {
 	free(global_config.topic);
 }
 
+static void opaque_check_kafka() {
+	void *o_meraki;
+	const char *value_a = NULL;
+	json_error_t jerr;
+	int ret_creator = 0;
+	int ret_reload = 0;
+	char err[512];
+
+	init_global_config();
+
+	const char MERAKI_OPAQUE[] = \
+            "{\"enrichment\":{\"a\":\"b\"}}";
+
+	global_config.topic = strdup("test");
+
+	if (NULL == global_config.rk) {
+		if (!(global_config.rk = rd_kafka_new(RD_KAFKA_CONSUMER, global_config.kafka_conf,
+								err, sizeof(err)))) {
+			fprintf(stderr,
+					"%% Failed to create new consumer: %s\n",
+					err);
+			exit(1);
+			}
+	}
+
+	json_t *opaque_config = json_loads(MERAKI_OPAQUE, 0, &jerr);
+
+	ret_creator = meraki_opaque_creator(opaque_config, &o_meraki);
+
+	/* without kafka config */
+	assert_int_equal(0, ret_creator);
+
+	assert(opaque_config);
+	const int unpack_rc = json_unpack_ex(opaque_config, &jerr, 0,
+	                                     "{s:{s:s}}",
+	                                     "enrichment",
+	                                     "a", &value_a);
+
+	assert(unpack_rc == 0);
+	assert(0 == strcmp(value_a, "b"));
+
+	meraki_opaque_cast(opaque_config);
+
+	if (ret_reload == 0)
+		meraki_opaque_destructor(o_meraki);
+
+	json_decref(opaque_config);
+
+	free(global_config.topic);
+	rd_kafka_destroy(global_config.rk);
+}
+
+
+
 static void mem_test(void (*cb)()) {
 	size_t i = 1;
 	do {
@@ -182,6 +236,10 @@ static void test_opaque1(){
 	opaque_check_no_kafka();
 }
 
+static void test_check_kafka(){
+	opaque_check_kafka();
+}
+
 static void test_opaque_mem_test(){
 	mem_test(opaque_check_no_kafka);
 }
@@ -205,7 +263,8 @@ int main() {
 		cmocka_unit_test(test_opaque_mem_test),
 		cmocka_unit_test(test_opaque_conf),
 		cmocka_unit_test(test_opaque_config_empty),
-		cmocka_unit_test(test_opaque_reload)
+		cmocka_unit_test(test_opaque_reload),
+		cmocka_unit_test(test_check_kafka)
 	};
 
 	return cmocka_run_group_tests(tests, NULL, NULL);
